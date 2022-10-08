5d13f4ff49cf2b13d1a541bc2e570d0a32be1040
Diego Vazquez <diego.vazquez@valoraapp.com>
2022-10-08 14:00:12 -0500

    Add pending transactions

--- contracts/TwoFactorAuthWallet.sol
+++ contracts/TwoFactorAuthWallet.sol
@@ -2,17 +2,36 @@
 
 pragma solidity ^0.8.0;
 
-///
 contract TwoFactorAuthWallet {
     address public owner;
     address public authenticatorSigner;
     uint256 public _nonce;
+
+    uint256 private timeToWaitForPendingTransactions = 1000;
+
+    PendingTransaction[] private _pendingTransactions;
 
     struct Signature {
         uint8 v;
         bytes32 r;
         bytes32 s;
     }
+
+    struct PendingTransaction {
+        address target;
+        uint256 value;
+        bytes data;
+        uint256 createdAt;
+        bool executed;
+    }
+
+    struct TransactionParams {
+        address target;
+        uint256 value;
+        bytes data;
+    }
+
+    event PendingTransactionAdded(PendingTransaction pendingTransation, uint256 index);  
 
     // solhint-disable-next-line no-empty-blocks
     receive() external payable {}
@@ -31,12 +50,39 @@ contract TwoFactorAuthWallet {
         return _nonce;
     }
 
-    function exec(
+    function revokePendingTransaction(uint256 index) public onlyOwner {
+        PendingTransaction storage pendingTransaction = _pendingTransactions[index];
+        pendingTransaction.executed = true;
+    }
+
+    function executePendingTransaction(uint256 index) public onlyOwner {
+        PendingTransaction storage pendingTransaction = _pendingTransactions[index];
+        require(!pendingTransaction.executed,
+            "Transaction already executed");
+        require(pendingTransaction.createdAt + timeToWaitForPendingTransactions < block.timestamp,
+            "Must wait before executing the transaction");
+        pendingTransaction.executed = true;
+        _call(
+            pendingTransaction.target,
+            pendingTransaction.value,
+            pendingTransaction.data);
+    }
+
+    function addPendingTransaction(
+        address target,
+        uint256 value,
+        bytes calldata data) public onlyOwner {
+        PendingTransaction memory pendingTransaction = PendingTransaction(target, value, data, block.timestamp, false);
+        _pendingTransactions.push(pendingTransaction);
+        emit PendingTransactionAdded(pendingTransaction, _pendingTransactions.length - 1);
+    }
+
+    function executeWithSignature(
         address target,
         uint256 value,
         bytes calldata data,
         Signature calldata signature
-    ) external onlyOwner {
+    ) public onlyOwner {
         bytes32 hashedMessage = keccak256(abi.encodePacked(target, value, data));
         require(authenticatorSigner == getSignerAddress(hashedMessage, signature), "Invalid signature");
         _call(target, value, data);
@@ -55,10 +101,11 @@ contract TwoFactorAuthWallet {
         }
     }
 
-    function getSignerAddress(bytes32 hashedMessage, Signature calldata signature) public pure returns (address) {
+    function getSignerAddress(bytes32 hashedMessage, Signature calldata signature) internal pure returns (address) {
         bytes memory prefix = "\x19Ethereum Signed Message:\n32";
         bytes32 prefixedHashMessage = keccak256(abi.encodePacked(prefix, hashedMessage));
         address signer = ecrecover(prefixedHashMessage, signature.v, signature.r, signature.s);
         return signer;
     }
+
 }
--- tests/TwoFactorAuthWallet.test.ts
+++ tests/TwoFactorAuthWallet.test.ts
@@ -1,56 +1,114 @@
 import Web3 from 'web3'
-import { ethers } from 'hardhat'
+import { ethers, network } from 'hardhat'
 import { expect } from 'chai'
 
 const target = '0x3078303030303030303030303030303030303031'
 const value = 0
-const data = 1234
+const data = '0x1234'
+
+const ZERO_IN_BYTES32 = '0x' + '0'.repeat(64)
 
 describe("Two Factor Auth Wallet", () => {
 
-  it('should execute the transaction if the signature is ok', async () => {
-    const [owner] = await ethers.getSigners();
+  describe('#executeWithSignature', () => {
+
     const signerAccount = createNewAccount()
     const packedMessage = ethers.utils.solidityPack(["address", "uint256", "bytes"], [target, value, data])
     const { v, r, s } = hashAndSignWithPrivateKey(packedMessage, signerAccount.privateKey)
 
-    const twoFactorAuthWallet = await ethers.getContractFactory("TwoFactorAuthWallet");
-    const twoFactorAuthWalletContract = await twoFactorAuthWallet.deploy(owner.address, signerAccount.address);
-    await twoFactorAuthWalletContract.deployed();
-
-    await twoFactorAuthWalletContract.exec(target, value, data, {v, r, s})
+    it('should execute the transaction if the signature is ok', async () => {
+      const [owner] = await ethers.getSigners();
+      const twoFactorAuthWallet = await ethers.getContractFactory("TwoFactorAuthWallet");
+      const twoFactorAuthWalletContract = await twoFactorAuthWallet.deploy(owner.address, signerAccount.address);
+      await twoFactorAuthWalletContract.deployed();
+  
+      await twoFactorAuthWalletContract.executeWithSignature(target, value, data, {v, r, s})
+    })
+  
+    it('should fail if the signature is from other account', async () => {
+      const [owner] = await ethers.getSigners();
+      const twoFactorAuthWallet = await ethers.getContractFactory("TwoFactorAuthWallet");
+      const twoFactorAuthWalletContract = await twoFactorAuthWallet.deploy(owner.address, owner.address);
+      await twoFactorAuthWalletContract.deployed();
+  
+      await (expect(twoFactorAuthWalletContract.executeWithSignature(target, value, data, {v, r, s})))
+        .to.be.revertedWith('Invalid signature')
+    })
+  
+    it('should fail if the sender is not the owner', async () => {
+      const [owner] = await ethers.getSigners();
+      const twoFactorAuthWallet = await ethers.getContractFactory("TwoFactorAuthWallet");
+      const twoFactorAuthWalletContract = await twoFactorAuthWallet.deploy(signerAccount.address, signerAccount.address);
+      await twoFactorAuthWalletContract.deployed();
+  
+      await (expect(twoFactorAuthWalletContract.executeWithSignature(target, value, data, {v, r, s})))
+        .to.be.revertedWith('2FA: Only owner')
+    })
   })
 
-  it('should fail if the signature is from other account', async () => {
-    const [owner] = await ethers.getSigners();
-    const signerAccount = createNewAccount()
-    const packedMessage = ethers.utils.solidityPack(["address", "uint256", "bytes"], [target, value, data])
-    const { v, r, s } = hashAndSignWithPrivateKey(packedMessage, signerAccount.privateKey)
+  describe('Pending transaction flow', () => {
+    it('emit PendingTransactionAdded events', async () => {
+      const [owner] = await ethers.getSigners();
+      const twoFactorAuthWallet = await ethers.getContractFactory("TwoFactorAuthWallet");
+      const twoFactorAuthWalletContract = await twoFactorAuthWallet.deploy(owner.address, owner.address);
+      await twoFactorAuthWalletContract.deployed();
 
-    const twoFactorAuthWallet = await ethers.getContractFactory("TwoFactorAuthWallet");
-    const twoFactorAuthWalletContract = await twoFactorAuthWallet.deploy(owner.address, owner.address);
-    await twoFactorAuthWalletContract.deployed();
+      const blockNumBefore = await ethers.provider.getBlockNumber();
+      const blockBefore = await ethers.provider.getBlock(blockNumBefore);
+      const currentTimestamp = blockBefore.timestamp + 1;
 
-    await (expect(twoFactorAuthWalletContract.exec(target, value, data, {v, r, s})))
-      .to.be.revertedWith('Invalid signature')
-  })
+      await expect(twoFactorAuthWalletContract.addPendingTransaction(target, value, data))
+      .to.emit(twoFactorAuthWalletContract, "PendingTransactionAdded").withArgs([
+        target, value, data, currentTimestamp, false
+      ], 0);
 
-  it('should fail if the sender is not the owner', async () => {
-    const [owner] = await ethers.getSigners();
-    const signerAccount = createNewAccount()
-    const packedMessage = ethers.utils.solidityPack(["address", "uint256", "bytes"], [target, value, data])
-    const { v, r, s } = hashAndSignWithPrivateKey(packedMessage, signerAccount.privateKey)
+      await expect(twoFactorAuthWalletContract.addPendingTransaction(target, value, data))
+      .to.emit(twoFactorAuthWalletContract, "PendingTransactionAdded").withArgs([
+        target, value, data, currentTimestamp + 1, false
+      ], 1);
+    })
 
-    const twoFactorAuthWallet = await ethers.getContractFactory("TwoFactorAuthWallet");
-    const twoFactorAuthWalletContract = await twoFactorAuthWallet.deploy(signerAccount.address, signerAccount.address);
-    await twoFactorAuthWalletContract.deployed();
+    it(`rejects the execution if time hasn't passed or already executed`, async () => {
+      const [owner] = await ethers.getSigners();
+      const twoFactorAuthWallet = await ethers.getContractFactory("TwoFactorAuthWallet");
+      const twoFactorAuthWalletContract = await twoFactorAuthWallet.deploy(owner.address, owner.address);
+      await twoFactorAuthWalletContract.deployed();
 
-    await (expect(twoFactorAuthWalletContract.exec(target, value, data, {v, r, s})))
-      .to.be.revertedWith('2FA: Only owner')
+      await twoFactorAuthWalletContract.addPendingTransaction(target, value, data)
+      
+      await expect(twoFactorAuthWalletContract.executePendingTransaction(0))
+        .to.be.revertedWith('Must wait before executing the transaction')
+
+      await passTime(2000)
+
+      await expect(twoFactorAuthWalletContract.executePendingTransaction(0))
+        .to.not.be.reverted
+
+        await expect(twoFactorAuthWalletContract.executePendingTransaction(0))
+        .to.be.revertedWith('Transaction already executed')
+    })
+
+    it(`rejects the execution if the transaction was revoked`, async () => {
+      const [owner] = await ethers.getSigners();
+      const twoFactorAuthWallet = await ethers.getContractFactory("TwoFactorAuthWallet");
+      const twoFactorAuthWalletContract = await twoFactorAuthWallet.deploy(owner.address, owner.address);
+      await twoFactorAuthWalletContract.deployed();
+
+      await twoFactorAuthWalletContract.addPendingTransaction(target, value, data)
+
+      await twoFactorAuthWalletContract.revokePendingTransaction(0)
+
+      await expect(twoFactorAuthWalletContract.executePendingTransaction(0))
+        .to.be.revertedWith('Transaction already executed')
+    })
   })
 
 });
 
+
+async function passTime(time: number) {
+  await network.provider.send("evm_increaseTime", [time])
+}
 
 function getWeb3() {
   return new Web3(new Web3.providers.HttpProvider(''))
